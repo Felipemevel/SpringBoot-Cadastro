2e80afa3df4f8440043a9a27027dcaa51522eb7f
Felipe Nascimento <lipe.conta1632@gmail.com>
2026-08-26 09:28:23 -0300

    Adicionado Lombok pra construtores, getters e setters

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -4,11 +4,15 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import dev.java10x.CadastroDeNinjas.Missoes.Dificuldades.Dificuldade;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name ="tb_missoes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissaoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,13 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,37 +20,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Chave Estrangeira
     private MissaoModel missaoVinculada;
 
-    public NinjaModel() {
-
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
